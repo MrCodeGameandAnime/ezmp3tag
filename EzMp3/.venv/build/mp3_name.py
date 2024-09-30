5ae4fd1efdb4aa20470fc6a32905c6c1793ab65f
Wesley Ruede <69434842+MrCodeGameandAnime@users.noreply.github.com>
2024-09-30 04:01:58 -0400

    updating ai services and implementing dotenv

--- EzMp3/.venv/build/mp3_name.py
+++ EzMp3/.venv/build/mp3_name.py
@@ -1,30 +1,5 @@
-'''
-import os
-from pathlib import Path
-from os import scandir
-
-def list_files(path):
-    for filename in os.listdir(path):
-        filepath = os.path.join(path, filename)
-        if os.path.isfile(filepath):
-            print(filename)
-
-def files_and_dirs():
-    for data in Path().rglob('*'):
-        if data.is_file():
-            print(data)
-
-            
-current_directory = os.getcwd()
-
-list_files(current_directory)
-print('break')
-files_and_dirs()
-'''
-
 import os
 import fnmatch
-global file_name
 
 def find_mp3_files(directory):
     """Searches the specified directory and subdirectories for mp3 files and returns a list of file paths."""
@@ -39,12 +14,8 @@ def find_mp3_files(directory):
 
     return mp3_files
 
-def set_file_name(value):
-    file_name = value
 
-def get_file_name():
-    return file_name
-
-directory_to_search = r'''C:\Users\User\Documents\Dev\Python\EzMp3\EzMp3\.venv\build\music_dir'''
-mp3_files = find_mp3_files(directory_to_search)
-file_name = mp3_files[0].strip('.mp3')
+def get_first_mp3():
+    directory_to_search = r'''C:\Users\User\Documents\Dev\Python\EzMp3\EzMp3\.venv\build\music_dir'''
+    mp3_files = find_mp3_files(directory_to_search)
+    return mp3_files[0].strip('.mp3')
